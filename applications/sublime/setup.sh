f1df1b26ea4e29801efa05002357f13bbac26505
Maddi Joyce <maddi@maddijoyce>
2018-12-16 09:01:31 +1100

    Sublime Fixes.

--- applications/sublime/setup.sh
+++ applications/sublime/setup.sh
@@ -7,4 +7,4 @@ ln -fs "$dotfiles_dir/applications/sublime/Preferences.sublime-settings" "$subli
 ln -fs "$dotfiles_dir/applications/sublime/Package Control.sublime-settings" "$sublime_preferences/Packages/User/Package Control.sublime-settings"
 
 mkdir -p "$sublime_preferences/Installed Packages"
-curl "https://packagecontrol.io/Package%20Control.sublime-package" -o "$sublime_preferences/Installed Packages/Package Control.sublime-package"
+wget "https://packagecontrol.io/Package%20Control.sublime-package" -P"$sublime_preferences/Installed Packages/"
