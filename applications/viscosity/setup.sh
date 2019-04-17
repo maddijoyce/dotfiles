865a441531f5febd055e877b07526830fca8e3f2
Maddi Joyce <maddi@maddijoyce.com>
2019-04-17 20:25:16 +1000

    Small fixes.

--- applications/viscosity/setup.sh
+++ applications/viscosity/setup.sh
@@ -4,5 +4,5 @@ viscosity_data="$HOME/Library/Application Support/Viscosity"
 
 ln -fs "$dotfiles_dir/applications/viscosity/com.viscosityvpn.Viscosity.plist" "$HOME/Library/Preferences/com.viscosityvpn.Viscosity.plist"
 
-mkdir -p $viscosity_data
+mkdir -p "$viscosity_data"
 ln -fs "$cloud_dir/OpenVPN" "$viscosity_data/OpenVPN"
