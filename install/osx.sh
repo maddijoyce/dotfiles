f5a8410f7f8fc3e23273c61141622a24778f0a44
Maddi Joyce <maddi@maddijoyce.com>
2019-10-22 15:41:50 +1100

    Push changes.

--- install/osx.sh
+++ install/osx.sh
@@ -105,9 +105,6 @@ defaults -currentHost write -g com.apple.trackpad.trackpadCornerClickBehavior -i
 defaults -currentHost write com.apple.trackpad.enableSecondaryClick -bool true
 defaults -currentHost write -globalDomain com.apple.mouse.tapBehavior -int 1
 
-# Set a really fast keyboard repeat rate.
-defaults write -g KeyRepeat -int 0
-
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
